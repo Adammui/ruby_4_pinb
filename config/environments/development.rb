3fdc9b9482453452096774174d14f7c08392de04
Adammui <adami.yuje@gmail.com>
2021-11-26 19:35:43 +0300

    configure mailer, it works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,17 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:'smtp.gmail.com',
+    port: 587,
+    domain:'gmail.com', #ENV[“GMAIL_DOMAIN”],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'weexfs@gmail.com', #ENV[“GMAIL_USERNAME”],
+    password: 'reyfindmetheway',#ENV[“GMAIL_PASSWORD”]
+  }
   # Do not eager load code on boot.
   config.eager_load = false
 

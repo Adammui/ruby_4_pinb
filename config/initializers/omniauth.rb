b49a44b276efbf6e488717451f34dfd0c268f9af
Adammui <adami.yuje@gmail.com>
2021-12-07 18:58:51 +0300

    ch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           Rails.application.credentials[:Client_ID],
-           Rails.application.credentials[:Client_secret]
+  provider :google_oauth2#,
+           #Rails.application.credentials[:Client_ID],
+           # Rails.application.credentials[:Client_secret]
 end
 OmniAuth.config.allowed_request_methods = %i[get]
 OmniAuth.config.allowed_request_methods = [:post, :get]

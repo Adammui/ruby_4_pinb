47f7e0eec11527199fb7e55480f196de2ecc5b89
Adammui <adami.yuje@gmail.com>
2022-04-25 19:45:17 +0300

    tryin to get password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:role_id , :id,:login, :full_name, :birthday, :email, :adress, :state, :city, :country, :zip)
+      params.require(:user).permit(:role_id , :id,:login, :full_name, :birthday, :email, :password)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :login, :full_name, :birthday, :email, :adress, :state, :city, :country, :zip, :password, :password_confirm, :created_at, :updated_at
+json.extract! user, :id, :login, :full_name, :birthday, :email, :password_confirm, :created_at, :updated_at
 json.url user_url(user, format: :json)
